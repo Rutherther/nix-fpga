e1921e0cf158099df3489e069ead10010302bcaa
Rutherther <rutherther@proton.me>
2024-03-23 14:18:45 +0100

    fix: ise udev

--- pkgs/xilinx/ise/udev.nix
+++ pkgs/xilinx/ise/udev.nix
@@ -1,4 +1,4 @@
-{ pkgs, fxload, ise-fw, ... }:
+{ lib, pkgs, fxload, ise-fw, ... }:
 
 pkgs.writeTextFile {
   name = "ise-udev-rules";
@@ -6,12 +6,12 @@ pkgs.writeTextFile {
   text = ''
     # version 0003
     ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0008", MODE="666"
-    SUBSYSTEM=="usb", ACTION=="add", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0007", RUN+="${fxload} -v -t fx2 -I ${ise-fw}/share/xusbdfwu.hex -D $tempnode"
-    SUBSYSTEM=="usb", ACTION=="add", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0009", RUN+="${fxload} -v -t fx2 -I ${ise-fw}/share/xusb_xup.hex -D $tempnode"
-    SUBSYSTEM=="usb", ACTION=="add", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="000d", RUN+="${fxload} -v -t fx2 -I ${ise-fw}/share/xusb_emb.hex -D $tempnode"
-    SUBSYSTEM=="usb", ACTION=="add", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="000f", RUN+="${fxload} -v -t fx2 -I ${ise-fw}/share/xusb_xlp.hex -D $tempnode"
-    SUBSYSTEM=="usb", ACTION=="add", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0013", RUN+="${fxload} -v -t fx2 -I ${ise-fw}/share/xusb_xp2.hex -D $tempnode"
-    SUBSYSTEM=="usb", ACTION=="add", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0015", RUN+="${fxload} -v -t fx2 -I ${ise-fw}/share/xusb_xse.hex -D $tempnode"
+    SUBSYSTEM=="usb", ACTION=="add", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0007", RUN+="${lib.getExe fxload} -v -t fx2 -i ${ise-fw}/share/xusbdfwu.hex -d $tempnode"
+    SUBSYSTEM=="usb", ACTION=="add", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0009", RUN+="${lib.getExe fxload} -v -t fx2 -i ${ise-fw}/share/xusb_xup.hex -d $tempnode"
+    SUBSYSTEM=="usb", ACTION=="add", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="000d", RUN+="${lib.getExe fxload} -v -t fx2 -i ${ise-fw}/share/xusb_emb.hex -d $tempnode"
+    SUBSYSTEM=="usb", ACTION=="add", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="000f", RUN+="${lib.getExe fxload} -v -t fx2 -i ${ise-fw}/share/xusb_xlp.hex -d $tempnode"
+    SUBSYSTEM=="usb", ACTION=="add", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0013", RUN+="${lib.getExe fxload} -v -t fx2 -i ${ise-fw}/share/xusb_xp2.hex -d $tempnode"
+    SUBSYSTEM=="usb", ACTION=="add", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0015", RUN+="${lib.getExe fxload} -v -t fx2 -i ${ise-fw}/share/xusb_xse.hex -d $tempnode"
 
     ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idVendor}=="03fd", MODE="666"
 
